0f3e84ef734e355b767c6f01caca8a97f334f89a
Sagar <sagargurung1001@gmail.com>
2025-01-01 16:29:42 +0545

    github pages
    Signed-off-by: Sagar <sagargurung1001@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -3,45 +3,13 @@ import {ImageText, ImageTextCoordinate } from "sagar-image-text";
 </script>
 <template>
   <div id="app">
-    <div class="wrapper">
-      <ImageText
-          imageSrc="sample.jpeg"
-          text="Mt Everest (8848m)"
-          text-position="bottom-left"
-          text-size="14px"
-      />
-      <ImageTextCoordinate
-          image-src="sample.jpeg"
-          text="Mt Everest (8848m)"
-          text-size="14px"
-          image_x_coordinate="27"
-          image_y_coordinate="75"
-      />
-    </div>
+    <h1>Sagar Text Image</h1>
+    <p>Usage demo will be provided soon :/</p>
   </div>
 </template>
 
 <style scoped>
-html, body {
-  height: 100%;
-  margin: 0;
-  padding: 0;
-  overflow: hidden;
-}
-
 #app {
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.wrapper {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  max-width: 100%;
-  max-height: 100%;
-  overflow: hidden;
+  text-align: center;
 }
 </style>
